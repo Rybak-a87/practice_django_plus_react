fc6ce4b4df3907b70dceb04f3f9a5cce021252c8
Rybak-a87 <rybak.a87@gmail.com>
2021-02-24 00:00:40 +0200

    lesson-7

---
+++ mainapp-ui/src/components/Navigation/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";   // useState и useEffect - хуки
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Navbar() {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "http://127.0.0.1:8000/api/category/"
+        }).then(response => {
+            setCategories(response.data)
+        })
+
+    }, [])
+
+  return (
+    <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="navbar-nav">
+
+                            {categories.map(c => (    // цикл
+                                <a className="nav-link" href="#" key={c.id}>{c.name}</a>
+                            ))}
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+  );
+}
+
+export default Navbar;
