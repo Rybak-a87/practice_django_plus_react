6f17359f46d2b2fb33f0175c9a653ca65fed2b18
Rybak-a87 <rybak.a87@gmail.com>
2021-02-23 23:44:27 +0200

    lesson-6

--- mainapp-ui/src/App.js
+++ mainapp-ui/src/App.js
@@ -2,28 +2,40 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap-grid.min.css"
 
 
 function App() {
-    const [people, setPeople] = useState([])
-
-    useEffect(() => {
-        axios({
-        method: "GET",
-        url: "http://127.0.0.1:8000/api/test-api/"
-    }).then(responce => {
-        setPeople(responce.data)
-    })
-  }, [])
 
   return (
     <div className="App">
-      <h1>Hello from React to Django</h1>
-        <ul>
-            {people.map(p => (
-                <li key={p.id}>{p.name}</li>
-            ))}
-        </ul>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
     </div>
   );
 }
